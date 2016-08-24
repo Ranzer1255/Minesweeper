fcb8dda77d9b920eca2d8d223828c7e6b012d14e
sgmaniac1255 <sgmaniac1255@yahoo.com>
2016-08-24 15:41:05 -0500

    add notes about printGrid()

--- src/minesweeper/IMinesweeperModel.java
+++ src/minesweeper/IMinesweeperModel.java
@@ -10,6 +10,10 @@ public interface IMinesweeperModel extends IFieldObservable{
 
 	public void flagCell(int x, int y);
 
+	/*
+	 * this might not be needed in the future. its only here for convenance
+	 * for the text based display
+	 */
 	public StringBuilder printGrid();
 	
 }
