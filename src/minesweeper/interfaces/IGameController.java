c3747e10735fbd05f9005a8f478bb4c787226a53
Bobby <sgmaniac1255@yahoo.com>
2017-08-20 00:09:00 -0500

    add comments and java docs
    maybe the *next* time i come to this code i'll actualy understand wtf i
    did here

--- src/minesweeper/interfaces/IGameController.java
+++ src/minesweeper/interfaces/IGameController.java
@@ -2,9 +2,22 @@ package minesweeper.interfaces;
 
 public interface IGameController extends IFieldObserver {
 	
+	/**
+	 * start a new game
+	 * will be called by the model but can be called
+	 * if called externally this should wipe the existing game
+	 */
 	public void newGame();
 	
+	/**
+	 * call back method when the game is won
+	 * will be called by the model
+	 */
 	public void gameWin();
 	
+	/**
+	 * callback method when the game is lost
+	 * will be called by the model
+	 */
 	public void gameLose();
 }
